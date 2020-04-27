0821bb27dff095afade9f069c7806d52ed0bb0e4
Samuel_Yeh <samuel.yeh01@gmail.com>
2020-04-27 18:05:47 +0800

    ::construction:: Work in progress-preparing the essential package for the project.
    詳述：
    準備開發環境所需之套件
    
    備忘：
    部分套件尚須研究，但先以前端畫面開發為主

--- app.js
+++ app.js
@@ -1,10 +1,119 @@
 const express = require("express");
-const handlebars = require("express-handlebars"); // 引入 handlebars
-const app = express();
-const port = 3000;
-app.engine("handlebars", handlebars()); // Handlebars 註冊樣板引擎
-app.set("view engine", "handlebars"); // 設定使用 Handlebars 做為樣板引擎
+const createError = require("http-errors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const Handlebar = require("handlebars");
+const bodyParser = require("body-parser");
+const favicon = require("express-favicon");
+const session = require("express-session");
+const i18n = require("i18n-express");
+const minify = require("express-minify");
+const passport = require("passport");
+const compression = require("compression");
+const Handlebars = require("./config/handlebars.config");
+// TODO:以下四個套件&資料夾之關係，待研究
+const dotenv = require("dotenv").config();
+const helpers = require("./helpers/loadHelpers");
+const firebaseDetails = require("./authentication/firebase/index");
+const auth0Details = require("./authentication/auth0/index");
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+const app = express();
+
+app.use(compression());
+
+const indexRouter = require("./routes/index");
+
+app.use(logger("dev"));
+
+app.use(cookieParser());
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "my session ID",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(__dirname + "/public/favicon.ico"));
+app.use(function (req, res, next) {
+  fullUrl = req.originalUrl;
+  split_path = fullUrl.split("/");
+  next();
 });
+
+// Handlebar URL helper
+Handlebar.registerHelper("url", function (link) {
+  return "/" + split_path[1] + link;
+});
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(
+  minify({
+    jsMatch: /javascript/,
+    jsMatch: /plugins/,
+    cssMatch: /css/,
+    cssMatch: /plugins/,
+  })
+);
+
+app.use(
+  i18n({
+    translationsPath: path.join(__dirname, "lang"), // use here. Specify translations files path.
+    siteLangs: [
+      "ar",
+      "en",
+      "cn",
+      "fr",
+      "he",
+      "es",
+      "ru",
+      "ge",
+      "ja",
+      "ko",
+      "it",
+      "hu",
+    ], // add new locales
+    cookieLangName: "ulang",
+    browserEnable: "true",
+    textsVarName: "translation",
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/", indexRouter);
+
+// Config Handlebars
+Handlebars(app, __dirname + "/views");
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error", {
+    title: "Node Xpress App",
+    layout: "auth",
+  });
+});
+
+module.exports = app;
