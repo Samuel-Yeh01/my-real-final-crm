465f6817d7258c8de62093ce5da8b8464efcf2d2
Samuel_Yeh <samuel.yeh01@gmail.com>
2020-05-08 00:10:34 +0800

    modify the instantsearch for ES6

--- public/plugins/instantSearch/instantsearch.js
+++ public/plugins/instantSearch/instantsearch.js
@@ -1,53 +1,55 @@
-'use strict';
+"use strict";
 /* global instantsearch */
 
-var search = instantsearch({
-   appId: 'latency',
-   apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-   indexName: 'instant_search',
-   routing: true
+const search = instantsearch({
+  appId: "latency",
+  apiKey: "6be0576ff61c053d5f9a3225e2a90f76",
+  indexName: "instant_search",
+  routing: true,
 });
 
 search.addWidget(
-   instantsearch.widgets.searchBox({
-      container: '#q',
-      placeholder: 'Search a product'
-   })
+  instantsearch.widgets.searchBox({
+    container: "#q",
+    placeholder: "Search a product",
+  })
 );
 
 search.addWidget(
-   instantsearch.widgets.stats({
-      container: '#stats'
-   })
+  instantsearch.widgets.stats({
+    container: "#stats",
+  })
 );
 
-search.on('render', function() {
-   $('.product-picture img').addClass('transparent');
-   $('.product-picture img').one('load', function() {
-      $(this).removeClass('transparent');
-   }).each(function() {
-      if(this.complete) $(this).load();
-   });
+search.on("render", function () {
+  $(".product-picture img").addClass("transparent");
+  $(".product-picture img")
+    .one("load", function () {
+      $(this).removeClass("transparent");
+    })
+    .each(function () {
+      if (this.complete) $(this).load();
+    });
 });
 
-var noResultsTemplate =
-   '<div class="text-center">No results found matching <strong>{{query}}</strong>.</div>';
+let noResultsTemplate =
+  '<div class="text-center">No results found matching <strong>{{query}}</strong>.</div>';
 
-var menuTemplate =
-   '<a href="javascript:void(0);" class="facet-item {{#isRefined}}active{{/isRefined}}"><span class="facet-name"><i class="fa fa-angle-right"></i> {{label}}</span class="facet-name"></a>';
+let menuTemplate =
+  '<a href="javascript:void(0);" class="facet-item {{#isRefined}}active{{/isRefined}}"><span class="facet-name"><i class="fa fa-angle-right"></i> {{label}}</span class="facet-name"></a>';
 
-var facetTemplateCheckbox =
-   '<a href="javascript:void(0);" class="facet-item">' +
-      '<input type="checkbox" class="{{cssClasses.checkbox}} filled-in" value="{{label}}" {{#isRefined}}checked{{/isRefined}} />{{label}}' +
-      '<span class="facet-count">({{count}})</span>' +
-   '</a>';
+let facetTemplateCheckbox =
+  '<a href="javascript:void(0);" class="facet-item">' +
+  '<input type="checkbox" class="{{cssClasses.checkbox}} filled-in" value="{{label}}" {{#isRefined}}checked{{/isRefined}} />{{label}}' +
+  '<span class="facet-count">({{count}})</span>' +
+  "</a>";
 
-var facetTemplateColors =
-   '<a href="javascript:void(0);" data-facet-value="{{label}}" class="facet-color {{#isRefined}}checked{{/isRefined}}"></a>';
+let facetTemplateColors =
+  '<a href="javascript:void(0);" data-facet-value="{{label}}" class="facet-color {{#isRefined}}checked{{/isRefined}}"></a>';
 
 search.addWidget(
   instantsearch.widgets.hits({
-    container: '#hits',
+    container: "#hits",
     hitsPerPage: 16,
     templates: {
       empty: noResultsTemplate,
@@ -79,120 +81,121 @@ search.addWidget(
                       </div>
                   {{/hits}}
               </div>`,
-            },
-      transformData: function(hit) {
-         hit.stars = [];
-         for (var i = 1; i <= 5; ++i) {
-         hit.stars.push(i <= hit.rating);
-         }
-         return hit;
+    },
+    transformData: function (hit) {
+      hit.stars = [];
+      for (var i = 1; i <= 5; ++i) {
+        hit.stars.push(i <= hit.rating);
       }
+      return hit;
+    },
   })
 );
 
 search.addWidget(
-   instantsearch.widgets.pagination({
-      container: '#pagination',
-      cssClasses: {
-         active: 'active'
-      },
-      labels: {
-         previous: '<i class="fa fa-angle-left fa-2x"></i> Previous page',
-         next: 'Next page <i class="fa fa-angle-right fa-2x"></i>'
-      },
-      showFirstLast: false
-   })
+  instantsearch.widgets.pagination({
+    container: "#pagination",
+    cssClasses: {
+      active: "active",
+    },
+    labels: {
+      previous: '<i class="fa fa-angle-left fa-2x"></i> Previous page',
+      next: 'Next page <i class="fa fa-angle-right fa-2x"></i>',
+    },
+    showFirstLast: false,
+  })
 );
 
 search.addWidget(
   instantsearch.widgets.hierarchicalMenu({
-    container: '#categories',
+    container: "#categories",
     attributes: [
-      'hierarchicalCategories.lvl0',
-      'hierarchicalCategories.lvl1',
-      'hierarchicalCategories.lvl2'
+      "hierarchicalCategories.lvl0",
+      "hierarchicalCategories.lvl1",
+      "hierarchicalCategories.lvl2",
     ],
-    sortBy: ['name:asc'],
+    sortBy: ["name:asc"],
     templates: {
-      item: menuTemplate
-    }
+      item: menuTemplate,
+    },
   })
 );
 
 search.addWidget(
   instantsearch.widgets.refinementList({
-    container: '#types',
-    attributeName: 'type',
-    operator: 'or',
+    container: "#types",
+    attributeName: "type",
+    operator: "or",
     limit: 5,
     templates: {
       item: facetTemplateCheckbox,
-      header: '<div class="facet-title mb-4">Category</div class="facet-title">'
-    }
+      header:
+        '<div class="facet-title mb-4">Category</div class="facet-title">',
+    },
   })
 );
 
 search.addWidget(
   instantsearch.widgets.refinementList({
-    container: '#brands',
-    attributeName: 'brand',
-    operator: 'or',
+    container: "#brands",
+    attributeName: "brand",
+    operator: "or",
     limit: 5,
     searchForFacetValues: true,
     templates: {
       item: facetTemplateCheckbox,
-      header: '<div class="facet-title mb-4">Brand</div class="facet-title">'
-    }
+      header: '<div class="facet-title mb-4">Brand</div class="facet-title">',
+    },
   })
 );
 
 search.addWidget(
   instantsearch.widgets.starRating({
-    container: '#rating',
-    attributeName: 'rating',
+    container: "#rating",
+    attributeName: "rating",
     templates: {
-      header: '<div class="facet-title mb-4">Rating</div class="facet-title">'
-    }
+      header: '<div class="facet-title mb-4">Rating</div class="facet-title">',
+    },
   })
 );
 
 search.addWidget(
   instantsearch.widgets.priceRanges({
-    container: '#prices',
-    attributeName: 'price',
+    container: "#prices",
+    attributeName: "price",
     cssClasses: {
-      list: 'nav nav-list',
-      count: 'badge pull-right',
-      active: 'active'
+      list: "nav nav-list",
+      count: "badge pull-right",
+      active: "active",
     },
     templates: {
-      header: '<div class="facet-title mb-4">Prices</div class="facet-title">'
-    }
+      header: '<div class="facet-title mb-4">Prices</div class="facet-title">',
+    },
   })
 );
 
 search.addWidget(
   instantsearch.widgets.sortBySelector({
-    container: '#sort-by-selector',
+    container: "#sort-by-selector",
     indices: [
-      {name: 'instant_search', label: 'Featured'},
-      {name: 'instant_search_price_asc', label: 'Price asc.'},
-      {name: 'instant_search_price_desc', label: 'Price desc.'}
+      { name: "instant_search", label: "Featured" },
+      { name: "instant_search_price_asc", label: "Price asc." },
+      { name: "instant_search_price_desc", label: "Price desc." },
     ],
-    label:'sort by'
+    label: "sort by",
   })
 );
 
 search.addWidget(
   instantsearch.widgets.clearAll({
-    container: '#clear-all',
+    container: "#clear-all",
     templates: {
-      link: '<i class="fa fa-eraser"></i> Clear all filters'
+      link: '<i class="fa fa-eraser"></i> Clear all filters',
     },
     cssClasses: {
-      root: 'btn btn-block btn-default'
+      root: "btn btn-block btn-default",
     },
-    autoHideContainer: true
+    autoHideContainer: true,
   })
 );
 
