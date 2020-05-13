15d7450a652a1134a0c415b838a3aec24941fd8a
Samuel_Yeh <samuel.yeh01@gmail.com>
2020-05-13 22:23:27 +0800

    :construction: Creat the CRUD of todo list
    詳述：
    建立todo list 的 CRUD 功能
    
    備忘：
    目前暫無法顯示所有 todo ，尚須研究!

--- controller/toDoController.js
+++ controller/toDoController.js
@@ -1,59 +1,33 @@
 const firebase = require("firebase");
 const db = firebase.firestore();
+const toDoListRef = db.collection("toDoList");
+const allToDo = toDoListRef
+  .get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log("Error getting documents", err);
+  });
 
 let toDoController = {
   // 瀏覽todo
   getToDo: (req, res) => {
-    // 調整 getCategories
-    db.collection("toDoList")
-      .where("id", ">=", 0)
-      .get()
-      .then(function (querySnapshot) {
-        querySnapshot.forEach(function (doc) {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
-        });
-      })
-      .catch(function (error) {
-        console.log("Error getting documents: ", error);
-      });
-
     res.render("widgets/toDoList", {
       parent: "Widgets",
       title: "toDoList",
       layout: "main",
-      // data: data,
+      data: allToDo,
     });
   },
-  // 新增分類
-  postCategory: (req, res) => {
-    categoryService.postCategory(req, res, (data) => {
-      if (data["status"] === "error") {
-        req.flash("error_messages", data["message"]);
-        return res.redirect("back");
-      }
-      req.flash("success_messages", data["message"]);
-      res.redirect("admin/categories", data);
-    });
-  },
-  // 新增 putCategory-也要加上負責修改資料 controller action：
-  putCategory: (req, res) => {
-    categoryService.putCategory(req, res, (data) => {
-      if (data["status"] === "error") {
-        req.flash("error_messages", data["message"]);
-        return res.redirect("back");
-      }
-      req.flash("success_messages", data["message"]);
-      res.redirect("admin/categories", data);
-    });
-  },
-  // 刪除分類
-  deleteCategory: (req, res) => {
-    categoryService.deleteCategory(req, res, (data) => {
-      if (data["status"] === "success") {
-        return res.redirect("admin/categories");
-      }
-    });
-  },
+  // 張貼todo
+  postToDo: (req, res) => {},
+  // 更新todo
+  putToDo: (req, res) => {},
+  // 刪除todo
+  deleteToDo: (req, res) => {},
 };
+
 module.exports = toDoController;
